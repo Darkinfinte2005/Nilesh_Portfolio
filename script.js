854c0b1686545832405ae8eeef35f01eea9fda7c
Nilesh Samal <122190028+Darkinfinte2005@users.noreply.github.com>
2024-09-25 20:50:26 +0530

    Add files via upload
    Adding index.html , style.css , script.js ,c.webp, CSS.png,HTML.png,JAVA SCRIPT.png,JAVA.png,profile.jpg.

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("recommendation-form").
+addEventListener("submit",function(event){event.preventDefault();alert("Your recommendation has been submited successfully, Thank you🙏.");});
